6c807e25b7118c7151a1c38a9fad6384616013b8
subin119 <sungman171@naver.com>
2016-11-29 13:54:13 +0900

    no message

--- src/main/java/com/ktdsuniv/user/web/UserController.java
+++ src/main/java/com/ktdsuniv/user/web/UserController.java
@@ -1,10 +1,12 @@
 package com.ktdsuniv.user.web;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.ktdsuniv.user.service.UserService;
 
 @Controller
+@RequestMapping("/instructor")
 public class UserController {
 
 	
@@ -13,5 +15,7 @@ public class UserController {
 	public void setUserService(UserService userService) {
 		this.userService = userService;
 	}
+	
+	
 	
 }
